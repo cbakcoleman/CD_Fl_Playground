9cfe6a42c0c5ab9c899086774dc3bc77ef27c154
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-02 13:26:04 -0700

    remove linked stylesheet, replace with inline css

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 @app.route('/play/<int:num>/<color>')
 def playground(num, color):
     return render_template("index.html", num = num, color = color)
 
-
-
-if__name__=="__main__":
-    app.run(debug=True)
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
